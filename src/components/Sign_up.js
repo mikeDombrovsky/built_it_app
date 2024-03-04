e63363011699ea304a8e96288a3382f7d4a2b2c7
Mikhail Dombrovsky <madombrovsky@gmail.com>
2024-03-04 23:47:07 +0200

    registration page

--- src/components/Sign_up.js
+++ src/components/Sign_up.js
@@ -1,46 +1,67 @@
+import {useAuth} from "../hooks/AuthProvider";
+import {useState} from "react";
+
 export const Sign_up = () => {
+    const {register} = useAuth();
+    const [inputs, setInputs] = useState({
+        username: '',
+        email: '',
+        password: ''
+    })
+    const {username, email, password} = inputs;
+
+    const onChange = e => {
+        setInputs({...inputs, [e.target.name]: e.target.value});
+    }
+
+    const onSubmit = e => {
+        e.preventDefault();
+        register(username, email, password);
+    }
+
     return (
-        <form>
-        <h3>Sign Up</h3>
-        <div className="mb-3">
-          <label>First name</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="First name"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>Last name</label>
-          <input type="text" className="form-control" placeholder="Last name" required/>
-        </div>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-            required
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-            required
-          />
-        </div>
-        <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
-            Sign Up
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Already registered <a href="/sign-in">sign in?</a>
-        </p>
-      </form>
+        <form onSubmit={onSubmit}>
+            <h3>Sign Up</h3>
+            <div className="mb-3">
+                <label>Username</label>
+                <input
+                    onChange={onChange}
+                    name="username"
+                    type="text"
+                    className="form-control"
+                    placeholder="Username"
+                />
+            </div>
+            <div className="mb-3">
+                <label>Email address</label>
+                <input
+                    onChange={onChange}
+                    name="email"
+                    type="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                    required
+                />
+            </div>
+            <div className="mb-3">
+                <label>Password</label>
+                <input
+                    onChange={onChange}
+                    name="password"
+                    type="password"
+                    className="form-control"
+                    placeholder="Enter password"
+                    required
+                />
+            </div>
+            <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                    Sign Up
+                </button>
+            </div>
+            <p className="forgot-password text-right">
+                Already registered <a href="/sign-in">sign in?</a>
+            </p>
+        </form>
     )
 }
--- src/hooks/AuthProvider.js
+++ src/hooks/AuthProvider.js
@@ -4,9 +4,37 @@ import Cookies from 'js-cookie';
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
-    const [token, setToken] = useState(()=> Cookies.get('token') ? Cookies.get('token') : null);
+    const [token, setToken] = useState(() => Cookies.get('token') ? Cookies.get('token') : null);
     const [refresh, setRefresh] = useState(() => Cookies.get('refresh') ? Cookies.get('refresh') : null);
     const [loading, setLoading] = useState(false);
+
+
+    const register = async (username = null, email, password) => {
+        try {
+            const response = await fetch('http://localhost:8000/api/register/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    email,
+                    password,
+                }),
+            });
+            if (response.ok) {
+                const data = await response.json();
+                console.log(data);
+                await logIn(email, password)
+            } else {
+                console.log("register failed");
+                const data = await response.json();
+                alert(JSON.stringify(data))
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
 
 
     const logIn = async (email, password) => {
@@ -95,16 +123,30 @@ const AuthProvider = ({children}) => {
     }
 
     const logOut = () => {
+
+        fetch('http://localhost:8000/api/token/logout/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                refresh: refresh,
+            }),
+        }).then(response => {
+            console.log(response.status);
+        }).catch(error => {
+            console.error('Error:', error);
+        })
+
         Cookies.remove('token');
         Cookies.remove('refresh');
         setToken(null);
         setRefresh(null);
-        console.log(token, refresh)
     };
 
     useEffect(() => {
-        let interval = setInterval(()=> {
-            if (refresh){
+        let interval = setInterval(() => {
+            if (refresh) {
                 refreshTokens()
             }
         }, 1000 * 60 * 55)
@@ -112,7 +154,7 @@ const AuthProvider = ({children}) => {
     }, [token, refresh, loading])
 
     return (
-        <AuthContext.Provider value={{token, refresh, logIn, refreshTokens, verifyToken, logOut}}>
+        <AuthContext.Provider value={{token, refresh, register, logIn, refreshTokens, verifyToken, logOut}}>
             {children}
         </AuthContext.Provider>
     );
